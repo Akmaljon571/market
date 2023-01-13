111917996c80a09dcf7a9f14075af6ff52c32fa2
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2023-01-13 10:18:15 +0500

    fixed products get

--- src/modules/products/products.ts
+++ src/modules/products/products.ts
@@ -1,17 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import postgres from '../../utils/postgres.js';
 import { allProducts } from './model.js';
 
 export default {
 	GET: async (req: Request, res: Response, next: NextFunction) => {
-		try {
-			const allProduct = postgres.fetchAll(allProducts);
-
-			console.log(allProduct);
-
-			res.json(await allProduct);
-		} catch (error) {
-			next(error);
-		}
+		res.json(await allProducts());
 	},
 };
